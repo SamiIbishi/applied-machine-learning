61ca64cf9efd22a64b38d5c121f10dd6ef01bdfe
MANUEL-XPS-13 <manuel.styrsky@gmx.de>
2020-12-04 17:39:25 +0100

    add comments

--- test/Pep8Validation.py
+++ test/Pep8Validation.py
@@ -5,7 +5,14 @@ import pep8
 
 
 class MyTestCase(unittest.TestCase):
-    def getFilePaths(self, rootdir="..\src"):
+    def getFilePaths(self, rootdir: str = "..\src"):
+        """
+        determines recursively all files of the given rootdir
+
+        :param rootdir: the dir where to start
+        :return: an array listing all filepaths
+        """
+
         paths = []
         dirpaths = os.listdir(rootdir)
         for path in dirpaths:
@@ -18,15 +25,21 @@ class MyTestCase(unittest.TestCase):
         return paths
 
     def test_pep8_conformance(self):
-        """Test that we conform to PEP8."""
+        """
+        Test that we conform to PEP8.
 
+        :return:
+        """
+
+        # setup
         paths = self.getFilePaths()
         pep8style = pep8.StyleGuide()
 
+        # execute
         result = pep8style.check_files(paths)
 
+        # validate
         self.assertEqual(result.total_errors, 0, "Found code style errors (and warnings).")
-
 
 if __name__ == '__main__':
     unittest.main()
