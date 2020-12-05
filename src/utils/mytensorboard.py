e5fcff7ac0fa5b8d65b9bb3fc84ce22fb932856c
MANUEL-XPS-13 <manuel.styrsky@gmx.de>
2020-12-05 23:46:04 +0100

    implemented method to start tensorboard if not already up

--- src/utils/mytensorboard.py
+++ src/utils/mytensorboard.py
@@ -1,6 +1,22 @@
 from torch.utils.tensorboard import SummaryWriter
 import os
+from socket import *
+import subprocess
 
+
+def pingit(host, port):  # defining function for later use
+
+    s = socket(AF_INET, SOCK_STREAM)  # Creates socket
+
+    try:
+        s.connect((host, port))  # tries to connect to the host
+    except ConnectionRefusedError:  # if failed to connect
+        s.close()  # closes socket, so it can be re-used
+        return False
+
+    while True:  # If connected to host
+        s.close()  # closes socket just in case
+        return True
 
 
 class MySummaryWriter(SummaryWriter):
@@ -12,8 +28,23 @@ class MySummaryWriter(SummaryWriter):
         self.base_logdir = base_logdir
         self.logpath = os.path.join(base_logdir, experiment_name, run_name)
         self.batch_size = batch_size
+        self.start_tensorboard()
         self.writer = SummaryWriter(self.logpath)
 
+    def start_tensorboard(self, host="localhost", port=6006):
+        """
+        If host and port cannot be pinged, this method starts tensorboard for the duration of the execution of this python programm
+        :param host:
+        :param port:
+        """
+        if pingit(host, port):
+            print("tensorboard is up: http://" + host + ":" + str(port))
+        else:
+            print("starting tensorboard...")
+            subprocess.Popen("tensorboard --logdir " + self.base_logdir)
+
+            print("tensorboard is temporary up: http://" + host + ":" + str(
+                port) + "\nWill be closed as soon as the python code exits. To run tensorboard independently execute '" + "tensorboard --logdir " + self.base_logdir + "'")
 
     def log_training_accuracy(self, acc, batch_index):
         index = self.epoch * self.numb_batches + batch_index
