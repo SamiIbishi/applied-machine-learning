7de0396f69ae0664a2f88931c8e1529c676736d2
Sami Ibishi <sami.ibishi@gmx.net>
2020-12-15 17:20:33 +0100

    update trainer and pipeline notebook

--- src/trainer/FaceNetTrainer.py
+++ src/trainer/FaceNetTrainer.py
@@ -8,8 +8,8 @@ import torchvision
 import time
 import typing
 
-from utils.mytensorboard import MySummaryWriter
-import utils.utils_images as img_util
+from src.utils.mytensorboard import MySummaryWriter
+#import src.utils.utils_images as img_util
 
 # Template to modify
 class SiameseNetworkTrainer:
@@ -36,7 +36,9 @@ class SiameseNetworkTrainer:
         self.optimizer = optimizer
         self.triplet_loss_func = triplet_loss_func
 
+        self.threshold = 1.0    # margin
         self.epoch = 0
+        self.epochs = 10
 
         # write to tensorboard
         self.tensorboard_writer = tensorboard_writer
@@ -53,16 +55,14 @@ class SiameseNetworkTrainer:
     def train_epoch(self):
         start_time = time.time()
         total_loss = 0
-        running_loss =0
-        for batch_idx, (images, ids) in enumerate(self.train_loader):
+        running_loss = 0
+        for batch_idx, (images, _) in enumerate(self.train_loader):
             # Get input from data loader
             anchor, positive, negative = images
-            target_ids = ids
 
             # Push tensors to GPU if available
             if torch.cuda.is_available():
                 anchor, positive, negative = anchor.cuda(), positive.cuda(), negative.cuda()
-                target_ids = target_ids.cuda()
 
             # Extract image embedding via model output
             anchor_output, positive_output, negative_output = self.model.forward(anchor,
@@ -93,18 +93,49 @@ class SiameseNetworkTrainer:
     def evaluate(self):
 
         self.tensorboard_writer.increment_epoch()
-        images, labels = next(iter(self.valid_loader))
-        predictions = ["1234", "1234", "1234", "1234"] #ToDO
-        fig = img_util.plot_classes_preds_face_recognition(images[0], labels, predictions)
 
-        self.tensorboard_writer.add_figure("predictions vs. actuals", fig, 1)
+        # switch to evaluate mode
+        self.model.eval()
+
+        correct_prediction = 0
+        for batch_idx, (images, ids) in enumerate(self.valid_loader):
+            # Get input from triplet 'images'
+            anchor, positive, negative = images
+            # (Nx3x512x512) => (NxEmbV) => (NxScalar)
+            # Push tensors to GPU if available
+            if torch.cuda.is_available():
+                anchor, positive, negative = anchor.cuda(), positive.cuda(), negative.cuda()
+
+            batch_size = anchor.shape[0]
+
+            # compute image embeddings
+            emb_anchor, emb_positive, emb_negative = self.model.forward(anchor, positive, negative)
+
+            # Distance between Anchor and Positive
+            dist_ap = torch.nn.functional.pairwise_distance(emb_anchor, emb_positive)
+
+            # Distance between Anchor and Negative
+            dist_an = torch.nn.functional.pairwise_distance(emb_anchor, emb_negative)
+
+            if (dist_ap <= self.threshold) & (dist_an >= self.threshold):
+                correct_prediction += 1
+
+            # if batch_idx == 0:
+            #     fig = img_util.plot_classes_preds_face_recognition(images[0], ids[0], predictions)
+            #     self.tensorboard_writer.add_figure("predictions vs. actuals", fig, 1)
+
+        # compute acc and log
+        valid_acc = (100. * correct_prediction) / len(self.valid_loader)
+        print(f'Validation accuracy: {valid_acc}')
+
+        return valid_acc
 
     def train(self, epochs: int = 10):
         self.epochs = epochs
-        for epoch in range(epochs):
+        for e in range(self.epochs):
             self.train_epoch()
             self.evaluate()
-            self.epoch +=1
+            self.epoch = e
 
     def inference(self, loader=None):
 
